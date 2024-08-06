44aca53cfb5c1769dfeba3174a6911e189bdfb95
DESKTOP-DFQSDU4\whxod <whxodid666@naver.com>
2024-08-06 23:53:09 +0900

    # lockfree queue 추가 중 # test code 수정

--- pkg/lockfree/queue/queue.go
+++ pkg/lockfree/queue/queue.go
@@ -1,6 +1,9 @@
 package queue
 
-import "sync/atomic"
+import (
+	"sync/atomic"
+	"unsafe"
+)
 
 type Node[T any] struct {
 	value T
@@ -8,7 +11,7 @@ type Node[T any] struct {
 }
 
 type TopNode[T any] struct {
-	next   *Node[T]
+	node   *Node[T]
 	unique int64
 }
 
@@ -19,15 +22,89 @@ type Queue[T any] struct {
 	count  atomic.Int32
 }
 
+func NewQueue[T any]() *Queue[T] {
+	q := Queue[T]{}
+	dummyNode := Node[T]{
+		next: nil,
+	}
+
+	head := TopNode[T]{
+		node:   &dummyNode,
+		unique: 0,
+	}
+
+	tail := TopNode[T]{
+		node:   head.node,
+		unique: 0,
+	}
+
+	q.count.Store(0)
+	q.unique.Store(0)
+	q.head.Store(&head)
+	q.tail.Store(&tail)
+
+	return &q
+}
+
 func (q *Queue[T]) Enqueue(val T) {
 	newNode := &Node[T]{value: val}
 	tmpUnique := q.unique.Add(1)
 	var tmpTail *TopNode[T]
+
 	for {
 		tmpTail = q.tail.Load()
 
-		if tmpTail.next.next == nil {
+		if tmpTail.node.next == nil {
+			if atomic.CompareAndSwapPointer(
+				(*unsafe.Pointer)(unsafe.Pointer(tmpTail.node.next)),
+				nil,
+				unsafe.Pointer(newNode)) {
+				if q.tail.CompareAndSwap(tmpTail, &TopNode[T]{
+					node:   tmpTail.node.next,
+					unique: tmpUnique,
+				}) {
+					break
+				}
+			}
+		} else {
+			q.tail.CompareAndSwap(tmpTail, &TopNode[T]{
+				node:   tmpTail.node.next,
+				unique: tmpUnique,
+			})
+		}
+	}
+	q.count.Add(1)
+}
 
+func (q *Queue[T]) Dequeue(val *T) {
+	var tmpTail *TopNode[T]
+	var tmpHead *TopNode[T]
+
+	if q.count.Add(-1) < 0 {
+		q.count.Add(1)
+		return
+	}
+
+	tmpUnique := q.unique.Add(1)
+	for {
+		tmpTail = q.tail.Load()
+		tmpHead = q.head.Load()
+		if tmpTail.node.next == nil {
+			if tmpHead.node.next != nil {
+				val = &tmpHead.node.value
+
+				if q.head.CompareAndSwap(tmpHead, &TopNode[T]{
+					node:   tmpHead.node.next,
+					unique: tmpUnique,
+				}) {
+
+				}
+			}
+		} else {
+			q.tail.CompareAndSwap(tmpTail, &TopNode[T]{
+				node:   tmpTail.node.next,
+				unique: tmpUnique,
+			})
 		}
 	}
 }
--- test/main.go
+++ test/main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/JoTaeYang/study-go/pkg/lockfree/lfstack"
+	"github.com/JoTaeYang/study-go/pkg/lockfree/queue"
 )
 
 const (
@@ -21,6 +22,78 @@ type Player struct {
 }
 
 var s *lfstack.Stack[*Player]
+var q *queue.Queue[*Player]
+
+func FuncTestRoutineQ() {
+	pList := make([]*Player, ARRAY_CNT, ARRAY_CNT)
+
+	var check bool
+	var i int
+	for {
+		for i = 0; i < ARRAY_CNT; i++ {
+			q.Dequeue(&pList[i])
+			if !check {
+				log.Println("Stack Is Empty")
+				panic("Stack Is Empty")
+			}
+
+			if pList[i] == nil {
+				log.Println("Player is Nil")
+				panic("Player is Nil")
+			}
+
+			if pList[i].Hp != 0x0000555 {
+				log.Println("Player Hp Is Not Init Data Equal")
+				panic("Player Hp Is Not Init Data Equal")
+			}
+
+			if pList[i].Att != 0 {
+				log.Println("Player Att Is Not Init Data Equal")
+				panic("Player Att Is Not Init Data Equal")
+			}
+		}
+		time.Sleep(0)
+
+		for i = 0; i < ARRAY_CNT; i++ {
+			atomic.AddInt32(&pList[i].Hp, 1)
+			atomic.AddInt32(&pList[i].Att, 1)
+		}
+
+		time.Sleep(0)
+		for i = 0; i < ARRAY_CNT; i++ {
+			if pList[i].Hp != 0x0000556 {
+				log.Println("Player Hp Is Not Init Data Equal")
+				panic("Player Hp Is Not Init Data Equal")
+			}
+
+			if pList[i].Att != 1 {
+				log.Println("Player Att Is Not Init Data Equal")
+				panic("Player Att Is Not Init Data Equal")
+			}
+		}
+
+		time.Sleep(0)
+		for i = 0; i < ARRAY_CNT; i++ {
+			atomic.AddInt32(&pList[i].Hp, -1)
+			atomic.AddInt32(&pList[i].Att, -1)
+		}
+
+		time.Sleep(0)
+		for i = 0; i < ARRAY_CNT; i++ {
+			if pList[i].Hp != 0x0000555 {
+				log.Println("Player Hp Is Not Init Data Equal")
+				panic("Player Hp Is Not Init Data Equal")
+			}
+
+			if pList[i].Att != 0 {
+				log.Println("Player Att Is Not Init Data Equal")
+				panic("Player Att Is Not Init Data Equal")
+			}
+
+			q.Enqueue(pList[i])
+		}
+	}
+}
 
 func FuncTestRoutine() {
 	pList := make([]*Player, ARRAY_CNT, ARRAY_CNT)
@@ -93,8 +166,7 @@ func FuncTestRoutine() {
 	}
 }
 func main() {
-	s = &lfstack.Stack[*Player]{}
-
+	q = queue.NewQueue[*Player]()
 	pList := make([]*Player, POOL_COUNT, POOL_COUNT)
 	for i := 0; i < POOL_COUNT; i++ {
 		pList[i] = &Player{
@@ -105,11 +177,11 @@ func main() {
 	}
 
 	for i := 0; i < POOL_COUNT; i++ {
-		s.Push(pList[i])
+		q.Enqueue(pList[i])
 	}
 
 	for i := 0; i < GO_CNT; i++ {
-		go FuncTestRoutine()
+		go FuncTestRoutineQ()
 	}
 
 	for {
@@ -120,4 +192,31 @@ func main() {
 
 		log.Println("lock free test ing ..")
 	}
+	// s = lfstack.NewStack[*Player]()
+
+	// pList := make([]*Player, POOL_COUNT, POOL_COUNT)
+	// for i := 0; i < POOL_COUNT; i++ {
+	// 	pList[i] = &Player{
+	// 		Hp:  0x0000555,
+	// 		Att: 0,
+	// 		Def: 5,
+	// 	}
+	// }
+
+	// for i := 0; i < POOL_COUNT; i++ {
+	// 	s.Push(pList[i])
+	// }
+
+	// for i := 0; i < GO_CNT; i++ {
+	// 	go FuncTestRoutine()
+	// }
+
+	// for {
+	// 	time.Sleep(time.Second)
+	// 	if s.GetCount() > POOL_COUNT {
+	// 		panic("pool count over")
+	// 	}
+
+	// 	log.Println("lock free test ing ..")
+	// }
 }
